06041868275935df901054f5ed57d8c246eeeada
rx3lixir <i027356@inbox.ru>
2025-11-14 20:18:42 +0700

    added minio methods

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.7.6
+	github.com/minio/minio-go/v7 v7.0.97
 	github.com/spf13/viper v1.21.0
 	github.com/valkey-io/valkey-go v1.0.68
 	golang.org/x/crypto v0.40.0
@@ -20,29 +21,41 @@ require (
 	github.com/charmbracelet/x/ansi v0.8.0 // indirect
 	github.com/charmbracelet/x/cellbuf v0.0.13-0.20250311204145-2c3ea96c31dd // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.11 // indirect
+	github.com/klauspost/crc32 v1.3.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/minio/crc64nvme v1.1.0 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/muesli/termenv v0.16.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/philhofer/fwd v1.2.0 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
+	github.com/rs/xid v1.6.0 // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	github.com/tinylib/msgp v1.3.0 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
+	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ pkg/storage/minio.go
@@ -0,0 +1,153 @@
+package storage
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+// MinIOClient wraps the MinIO client for voice message storage
+type MinIOClient struct {
+	client     *minio.Client
+	bucketName string
+}
+
+// NewMinIOClient creates a new MinIO client and ensures bucket exists
+func NewMinIOClient(endpoint, accessKey, secretKey, bucketName string, useSSL bool) (*MinIOClient, error) {
+	client, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
+		Secure: useSSL,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to create minio client: %w", err)
+	}
+
+	mc := &MinIOClient{
+		client:     client,
+		bucketName: bucketName,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := mc.ensureBucket(ctx); err != nil {
+		return nil, err
+	}
+
+	return mc, nil
+}
+
+// ensureBucket creates the bucket if it doesn't exist
+func (m *MinIOClient) ensureBucket(ctx context.Context) error {
+	exists, err := m.client.BucketExists(ctx, m.bucketName)
+	if err != nil {
+		return fmt.Errorf("failed to check bucket existence: %w", err)
+	}
+
+	if !exists {
+		err = m.client.MakeBucket(ctx, m.bucketName, minio.MakeBucketOptions{})
+		if err != nil {
+			return fmt.Errorf("failed to create bucket: %w", err)
+		}
+	}
+
+	return nil
+}
+
+// UploadVoiceMessage uploads a voice message file to MinIO
+// Returns the object path in MinIO
+func (m *MinIOClient) UploadVoiceMessage(
+	ctx context.Context,
+	messageID uuid.UUID,
+	data []byte,
+	audioFormat string,
+) (string, error) {
+	// Format: messages/YYYY/MM/DD/messageID.format
+	now := time.Now()
+	objectName := fmt.Sprintf(
+		"messages/%d/%02d/%02d/%s.%s",
+		now.Year(),
+		now.Day(),
+		now.Month(),
+		messageID.String(),
+		audioFormat,
+	)
+
+	// Determine content type based on format
+	contentType := "audio/opus"
+	switch audioFormat {
+	case "mp3":
+		contentType = "audio/mpeg"
+	case "ogg":
+		contentType = "audio/ogg"
+	case "wav":
+		contentType = "audio/wav"
+	}
+
+	// Upload the file
+	reader := bytes.NewReader(data)
+	_, err := m.client.PutObject(
+		ctx,
+		m.bucketName,
+		objectName,
+		reader,
+		int64(len(data)),
+		minio.PutObjectOptions{
+			ContentType: contentType,
+		},
+	)
+	if err != nil {
+		return "", fmt.Errorf("failed to upload to minio: %w", err)
+	}
+
+	return objectName, nil
+}
+
+// DownloadVoiceMessage downloads a voice message from MinIO
+func (m *MinIOClient) DownloadVoiceMessage(ctx context.Context, objectName string) ([]byte, error) {
+	object, err := m.client.GetObject(ctx, m.bucketName, objectName, minio.GetObjectOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get object: %w", err)
+	}
+	defer object.Close()
+
+	data, err := io.ReadAll(object)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read object: %w", err)
+	}
+
+	return data, nil
+}
+
+// DeleteVoiceMessage deletes a voice message from MinIO
+func (m *MinIOClient) DeleteVoiceMessage(ctx context.Context, objectName string) error {
+	err := m.client.RemoveObject(ctx, m.bucketName, objectName, minio.RemoveObjectOptions{})
+	if err != nil {
+		return fmt.Errorf("failed to delete object: %w", err)
+	}
+	return nil
+}
+
+// DeleteVoiceMessage deletes a voice message from MinIO
+func (m *MinIOClient) GetPresignedURL(ctx context.Context, objectName string, expiry time.Duration) (string, error) {
+	url, err := m.client.PresignedGetObject(ctx, m.bucketName, objectName, expiry, nil)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate presigned url: %w", err)
+	}
+	return url.String(), nil
+}
+
+// GetObjectInfo retrieves metadata about a stored object
+func (m *MinIOClient) GetObjectInfo(ctx context.Context, objectName string) (*minio.ObjectInfo, error) {
+	info, err := m.client.StatObject(ctx, m.bucketName, objectName, minio.StatObjectOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get object info: %w", err)
+	}
+	return &info, nil
+}
